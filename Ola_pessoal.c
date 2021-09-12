5f6ebebac4c76115a71d645fe877114b9e0d9a16
mafone <40169459+mafone@users.noreply.github.com>
2021-09-12 15:59:21 +0100

    Update Ola_pessoal.c

--- Ola_pessoal.c
+++ Ola_pessoal.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+//Cabeçalho
+#include <stdio.h> //Incluir a biblioteca que permite usar o printf
 
+//Programa principal
 void main()
-{
-    printf("Olá mundo!");
-}
+{ //Início
+    printf("Olá mundo!"); //Mostrar mensagem de boas vidas
+} //Fim
